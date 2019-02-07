25fc4dc98a3c17ec914a3c57a6cf1557dd69a511
dzhord <45306692+dzhord@users.noreply.github.com>
2019-02-07 04:41:48 +0300

    Update product_from_site.py
    add parse product, but not all

--- product_from_site.py
+++ product_from_site.py
@@ -1,21 +1,54 @@
 import requests
+from bs4 import BeautifulSoup
 
-
-def download_product():
+def get_html(url):
+    #https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty?p=2
     headers = {'User-agent': 'Mozilla/5.0'}
     param = { 'p': '1'}
     SITE_URL = "https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty"
     try:
         result = requests.get(SITE_URL, headers=headers, params=param)
-        data = result.text
-        print(result)
-        return data
+        return result.text
     except(requests.exceptions.ConnectionError):
         print("No connection")
         return False
 
+def get_total_pages(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    pages = soup.find('div', class_='pagination-pages clearfix').find_all('a', class_='pagination-page')[-1].get('href')
+    total_pages = pages.split('=')[1]
+    return int(total_pages)
+    
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    ads = soup.find('div', class_='catalog-list').find_all('div', class_='description item_table-description')
+    # Невыципил данные об товарах
+    s = len(ads)
+    print(ads)
+    print(s)
+
+def get_html_product():
+    url = "https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty?p=1"
+    base_url = "https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty?"
+    page_part = 'p='
+    
+    total_pages = get_total_pages(get_html(url))
+
+    for i in range(1,3):
+        url_gen = base_url + page_part + str(i)
+        html = get_html(url_gen)
+        get_page_data(html)
+
+    return total_pages
+
 if  __name__ == "__main__":
-    html = download_product()
+    get_html_product()
+
+'''
     if html:
         with open("avito.html", "w", encoding="utf8") as ff:
             ff.write(html)
+'''
+
